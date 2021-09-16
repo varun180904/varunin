a4f1970d14e1edd0915201868e18ee952f7d834a
varun180904 <68421123+varun180904@users.noreply.github.com>
2021-09-16 12:19:53 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var http = require("http");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
