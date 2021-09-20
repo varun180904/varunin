66de61a78cb9a48a62406eff872ee175aafa2f4d
varun180904 <68421123+varun180904@users.noreply.github.com>
2021-09-20 11:39:09 +0530

    Update node.js

--- webapp/src/main/webapp/node.js
+++ webapp/src/main/webapp/node.js
@@ -1,6 +1,11 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World1 !');
-}).listen(8080);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
